6711658f034aba7de1207724c6860a4efeae5323
Moises Castro <moisesgithub@gmail.com>
2026-07-25 22:25:03 -0300

    Criando controller

---
+++ src/main/java/com/moisegit/to_do_list/controller/AutorController.java
@@ -0,0 +1,38 @@
+package com.moisegit.to_do_list.controller;
+
+import com.moisegit.to_do_list.dto.AutorDto;
+import com.moisegit.to_do_list.model.AutorEntity;
+import com.moisegit.to_do_list.repository.AutorRepository;
+import com.moisegit.to_do_list.service.AutorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/autor")
+@RequiredArgsConstructor
+public class AutorController {
+
+    private final AutorRepository autorRepository;
+    private final AutorService autorService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createAutor(@RequestBody AutorDto autorDto) {
+        autorService.cadastrarAutor(autorDto);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    List<AutorEntity> findAll() {
+        return autorRepository.findAll();
+    }
+
+    @GetMapping("/nome/{nome}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<AutorDto> findByNome(@PathVariable String nome) {
+        return autorService.findByNome(nome);
+    }
+}
--- src/main/java/com/moisegit/to_do_list/repository/AutorRepository.java
+++ src/main/java/com/moisegit/to_do_list/repository/AutorRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AutorRepository extends JpaRepository<AutorEntity, Integer> {
-    List<AutorDto> findByNome(String nome);
-    List<AutorDto> findByNacionalidade(String nacionalidade);
+    List<AutorEntity> findByNome(String nome);
+    List<AutorEntity> findByNacionalidade(String nacionalidade);
 }
